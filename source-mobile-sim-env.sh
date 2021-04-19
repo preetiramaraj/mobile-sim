a2c176074510c00c9383b1956a379afddbf7b3bd
Aaron Mininger <mininger@umich.edu>
2021-04-19 17:13:15 -0400

    Removed unnecessary pieces of april

--- source-mobile-sim-env.sh
+++ source-mobile-sim-env.sh
@@ -8,6 +8,13 @@ export CLASSPATH=$CLASSPATH:$MOBILE_SIM_HOME/java/mobilesim.jar
 export PYTHONPATH=$PYTHONPATH:$MOBILE_SIM_HOME/python
 
 
+### APRIL Environment Variables
+export APRIL_HOME=$MOBILE_SIM_HOME/april
+export CLASSPATH=$CLASSPATH:$APRIL_HOME/java/april.jar
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$APRIL_HOME/lib:$APRIL_HOME/java/jni/jgl
+alias java='java -ea -server'
+
+
 ### Setup Helper Scripts
 
 source $MOBILE_SIM_HOME/scripts/custom_completions.sh
